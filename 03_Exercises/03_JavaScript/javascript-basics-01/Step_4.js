0ca3db714e695be9562d4c83f3bed426d6fdad86
Omar Khankan <omar.khankan@gmail.com>
2022-12-07 14:16:59 +0000

    Step_4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+function Person(){
+    var fname= document.getElementById("name").value;
+var surname=document.getElementById("surname").value;
+var city=document.getElementById("city").value;
+    alert("Name: "+fname+ '\n'+"Surname: "+surname+'\n'+"City: "+city);
+}
