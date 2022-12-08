9110949ef11a0922c33c794359cebe388df77b53
Omar Khankan <omar.khankan@gmail.com>
2022-12-08 22:58:47 +0000

    Step-3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step-3.js
@@ -0,0 +1,11 @@
+var text=document.getElementById('text');
+
+function Red(){
+  text.style='color:red';
+}
+function Green(){
+    text.style='color:green';
+}
+function Blue(){
+   text.style='color:blue';
+}
