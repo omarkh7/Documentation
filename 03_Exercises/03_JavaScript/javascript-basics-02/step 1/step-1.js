b05b2bbf4c50280116f325df44a93546f3ea50de
Omar Khankan <omar.khankan@gmail.com>
2022-12-08 10:32:10 +0000

    Step_1

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step-1.js
@@ -0,0 +1,7 @@
+var image1;
+function mouseover(){
+    image1 = document.querySelector("#image1").src="./images/image1_2.jpg";
+}
+function mouseout(){
+     image1 = document.querySelector("#image1").src="./images/image1.jpg";
+}
