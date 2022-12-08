61e43f0cec77a1454950f4fc32e9802f38d0c56b
Omar Khankan <omar.khankan@gmail.com>
2022-12-08 08:22:42 +0000

    Step_8

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function Age(){
+    var age = document.getElementById("age").value;
+ 
+    if(age >= 18){
+      alert("you are over 18");
+    }
+    else{
+     alert("you are under 18");
+    }
+}
