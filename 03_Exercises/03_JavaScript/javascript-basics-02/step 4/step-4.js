60d869ca872e23d642a4b36d48889949d03f62ed
Omar Khankan <omar.khankan@gmail.com>
2022-12-08 15:06:06 +0000

    Step_4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step-4.js
@@ -0,0 +1,12 @@
+
+
+function Reset1() {
+var yes= confirm("do you want to Clear the form");
+if(yes){
+document.getElementById("name").value="";
+document.getElementById("surname").value="";
+document.getElementById("city").value="";
+}
+
+}
+ 
