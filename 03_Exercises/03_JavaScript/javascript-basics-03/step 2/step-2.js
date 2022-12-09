2f755cbcd1d8217343ec39bd36a2863cd34744d2
Omar Khankan <omar.khankan@gmail.com>
2022-12-09 11:37:21 +0000

    Changing to 1 function

--- 03_Exercises/03_JavaScript/javascript-basics-03/step 2/step-2.js
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/step-2.js
@@ -1,16 +1,26 @@
 var texte = document.getElementById('texte');
 
-function Show() {
-texte.style='display:block'
-}
+// function Show() {
+// texte.style='display:block'
+// }
 
-function Hide(){
-texte.style='display:none';
-}
-
-
+// function Hide(){
+// texte.style='display:none';
+// }
 
 
+
+function Show(showhide){
+ 
+    var texte = document.getElementById("texte");
+        if(showhide=='show'){
+            texte.style='display:block'
+        }
+        else if (showhide=="hide"){
+            texte.style='display:none';
+        }
+      
+    }
 
 
 
