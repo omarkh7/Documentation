7c980f65a9b04159387429f4714cc0187338e8d6
Omar Khankan <omar.khankan@gmail.com>
2022-12-11 11:08:35 +0000

    Basic Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,7 @@
+1-SELECT name FROM students;
+2-SELECT * FROM students WHERE Age>'30';
+3-SELECT name FROM students WHERE Gender='F' AND Age='30';
+4-SELECT Points FROM students WHERE name='Alex';
+5-INSERT INTO students  VALUES(7,'Omar',24,'M',800);
+6-UPDATE students SET Points=400 WHERE id=2;
+7-UPDATE students SET Points=100 WHERE id=1;
