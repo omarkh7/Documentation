a32e2df6a52ab2c7c530d066b40f6d857828a947
Omar Khankan <omar.khankan@gmail.com>
2023-01-05 15:45:07 +0200

    help command

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -1,50 +1,48 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`\nWelcome to ${name}'s application!`);
+  console.log("\nFor Help type help");
+  console.log("--------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n' || text === 'exit\n') {
+  if (text === "quit\n" || text === "exit\n") {
     quit();
-  }
-  else if(text === 'hello\n'){
+  } else if (text === "hello\n") {
     hello();
-  }
-  else{
+  } else if (text === "help\n") {
+    help();
+  } else {
     unknownCommand(text);
   }
 }
-
 
 /**
  * prints "unknown command"
@@ -53,30 +51,38 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello() {
+  console.log("\nhello!\n");
 }
-
 
 /**
  * Exits the application
  *
  * @returns {void}
  */
-function quit(){
-  console.log('Quitting now, goodbye!')
+function quit() {
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
+/**
+ * Help
+ *
+ * @returns {void}
+ */
+function help() {
+  console.log("\nEnter hello to print hello!\n");
+  console.log("Enter quit or exit to exit the application\n");
+}
+
 // The following line starts the application
-startApp("Omar Khankan")
+startApp("Omar Khankan");
