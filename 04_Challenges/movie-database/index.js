9de7abda59b6565b4ea2628bbf172358d1dac2f8
Omar Khankan <omar.khankan@gmail.com>
2023-01-07 20:44:56 +0200

    step 3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-    res.send('ok')
-  })
-  
-  app.listen(port, () => {
-    console.log("ok")
-  })
+const express = require("express");
+const app = express();
+const port = 3000;
+const times = new Date();
+let hours = times.getHours();
+let minutes = times.getMinutes();
+let time = hours + ":" + minutes;
+app.get("/", ( res) => {
+  res.send("ok");
+});
+app.get("/test", (res) => {
+  res.send({ status: 200, message: time });
+});
+
+app.listen(port, () => {
+  console.log("ok");
+});
