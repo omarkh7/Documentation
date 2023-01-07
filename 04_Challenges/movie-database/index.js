08f8a61a2ca321ef14a38b28bdf7a565ca9b2899
Omar Khankan <omar.khankan@gmail.com>
2023-01-07 20:05:08 +0200

    step 2

---
+++ 04_Challenges/movie-database/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    res.send('ok')
+  })
+  
+  app.listen(port, () => {
+    console.log("ok")
+  })
