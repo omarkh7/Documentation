3e7ed7013150f47165f22d525a2e98107a575d14
Omar Khankan <omar.khankan@gmail.com>
2023-01-07 21:21:35 +0200

    step 5

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -7,6 +7,15 @@ let hours = times.getHours();
 let minutes = times.getMinutes();
 let time = hours + ":" + minutes;
 
+//database
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+
 app.get("/", (req,res) => {
   res.send("ok");
 });
@@ -39,6 +48,20 @@ app.get('/hello/:id', (req, res) => {
         message: 'you have to provide a search'
       });
     }
+  });
+
+  //Movies
+  app.get("/movies/add", (req,res) => {
+    res.send({ status: 200, message:"" });
+  });
+  app.get("/movies/get", (req,res) => {
+    res.send({ status: 200, data: movies });
+  });
+  app.get("/movies/edit", (req,res) => {
+    res.send({ status: 200, message: "" });
+  });
+  app.get("/movies/delete", (req,res) => {
+    res.send({ status: 200, message: "" });
   });
 
 app.listen(port, () => {
